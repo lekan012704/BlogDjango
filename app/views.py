cbd9e251da5e4da2e596706da4a875efc2e6a176
lekan012704 <durosinmiabdulsobuur>
2022-05-19 11:29:25 +0100

    NEW ONES

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
+from tempfile import template
 from django.shortcuts import render
 
+def Home(request):
+    template_name = 'index.html'
+    return render(request,template_name)
 # Create your views here.
